23f80225999f57bbe4be8e8eb41ff245504d6eb7
chrisley75 <cley@paloaltonetworks.com>
2024-09-17 23:57:48 +0200

    update prisma cloud api v33.00 url

--- ansible/scripts/download_daemonset.sh
+++ ansible/scripts/download_daemonset.sh
@@ -4,7 +4,7 @@ pc_twistlock_end_point_url=$1
 pc_token=$2
 pc_console_url=$3
 
-curl -s --location ${pc_twistlock_end_point_url}/api/v32.07/defenders/daemonset.yaml --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'x-redlock-auth: '${pc_token}'' --data '{
+curl -s --location ${pc_twistlock_end_point_url}/api/v33.00/defenders/daemonset.yaml --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'x-redlock-auth: '${pc_token}'' --data '{
    "orchestration": "container",
    "consoleAddr": "'${pc_console_url}'",
    "containerRuntime": "containerd",
